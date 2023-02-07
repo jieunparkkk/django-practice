808633c1d1adb93483b4b9c3914700ef38e42696
chtw2001 <jungtaekwon1019@gmail.com>
2023-02-07 18:19:53 +0900

    Todo 수정 페이지와 기능 만들기

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.Todos),
     # 클래스를 불러오기위해 .as_view()함수를 붙임
     path('create_todo/', views.TodoCreate.as_view()),
+    path('update_todo/<int:pk>/', views.TodoUpdate.as_view()),
 
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from . models import Todo
+
+class TodoUpdate(UpdateView):
+    model = Todo
+    fields = ['todo', 'description', 'important']
+
+    template_name = 'todo_app/todo_update_form.html'
+
 
 class TodoCreate(LoginRequiredMixin, CreateView):
     model = Todo
