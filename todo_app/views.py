df5b12acd924377ca2ed5f5516f54b046a9b36c2
chtw2001 <jungtaekwon1019@gmail.com>
2023-02-07 16:41:07 +0900

    할 일 목록 페이지 만들기

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from . models import Todo
 
-# Create your views here.
+def index(request):
+    todos = Todo.objects.all().order_by('pk')
+
+    return render(
+        request, 
+        'todo_app/index.html', 
+        {'todos':todos},
+        
+        )
