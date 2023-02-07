e4e5362bf7f6dca0d5f70ad0ffe93423cad8f1ed
chtw2001 <jungtaekwon1019@gmail.com>
2023-02-07 17:11:31 +0900

    부트스트랩과 템프릿 적용하기

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from . models import Todo
 
-def index(request):
+def Todos(request):
     todos = Todo.objects.all().order_by('pk')
 
     return render(
         request, 
-        'todo_app/index.html', 
+        'todo_app/todos.html', 
         {'todos':todos},
         
         )
