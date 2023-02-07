3fc4261828b516f646db4e80045c77a1d724c909
chtw2001 <jungtaekwon1019@gmail.com>
2023-02-07 18:12:04 +0900

    Todo 생성 페이지와 기능 만들기

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Todos),
+    # 클래스를 불러오기위해 .as_view()함수를 붙임
+    path('create_todo/', views.TodoCreate.as_view()),
+
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from . models import Todo
+
+class TodoCreate(LoginRequiredMixin, CreateView):
+    model = Todo
+    fields = ['todo', 'description', 'important']
+
 
 def Todos(request):
     todos = Todo.objects.all().order_by('pk')
